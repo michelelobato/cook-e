24ed923d04bbf5a28ebbe3228f6b34cce5ac1b9c
michelelobato <148112622+michelelobato@users.noreply.github.com>
2023-11-16 15:33:19 -0700

    Add files via upload

---
+++ cookie/public_html/client.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+   
+        //press login button
+        document.getElementById('loginButton').addEventListener('click', function(e) {
+            e.preventDefault();
+                    
+                    window.location.href = 'login.html';
+            
+        });
+        document.getElementById('signUpButton').addEventListener('click', function(e) {
+            e.preventDefault();
+                 
+                    window.location.href = 'signup.html';
+            
+        });
+        document.getElementById('helpButton').addEventListener('click', function(e) {
+            e.preventDefault();
+                 
+                    window.location.href = 'signup.html';
+            
+        });
+    
+});
+
