c7340f7e755d6db0a8c5d45f4f283ff1bb86f20c
Dylan Crites <llamacornzftw@gmail.com>
2023-11-27 12:55:05 -0700

    new file:   cookie/public_html/TODO 	modified:   cookie/public_html/client.js modified:   cookie/public_html/homestyle.css new file:   cookie/public_html/mongoInfo

--- cookie/public_html/client.js
+++ cookie/public_html/client.js
@@ -1,3 +1,27 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const crypto = require('crypto');
+
+//TODO fix this connection string
+const connection_string = 'mongodb://127.0.0.1/temporary';
+
+mongoose.connect(connection_string);
+mongoose.connection.on('error', () => {
+  console.log('There was a problem connecting to mongoDB');
+});
+
+var UserSchema = new mongoose.Schema({
+        username: String,
+        passwordHash: String,
+      });
+var User = mongoose.model('User', UserSchema );
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
    
         //press login button
