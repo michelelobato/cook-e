47a2de5b20e29bb1cf938f1de34d2cb2f073f022
Dylan Crites <llamacornzftw@gmail.com>
2023-11-27 17:42:27 -0700

    modified:   cookie/public_html/TODO modified:   cookie/public_html/client.js modified:   cookie/public_html/home.html

--- cookie/public_html/client.js
+++ cookie/public_html/client.js
@@ -13,11 +13,69 @@ mongoose.connection.on('error', () => {
 });
 
 var UserSchema = new mongoose.Schema({
+        fullname: String,
         username: String,
         passwordHash: String,
+        phoneNumber: [{type: String, number: String}],
+        emails: [{email: String, order: Number}]
+        
       });
 var User = mongoose.model('User', UserSchema );
 
+var restaurantSchema = new mongoose.Schema({
+        restaurantName: String,
+        /*adminUserNames: [String],*/
+        userName: [String],
+        /*passwordHash: String */
+        menu: String,
+        imgNames: [{fileName: String, order: Number}],
+        hours: [{day: String, open: String, close: String}],
+        phoneNumber: [{type: String, number: String}],
+        emails: [{email: String, order: Number}],
+        address: String,
+        website: String
+});
+var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+var homePageSchema = new mongoose.Schema({
+        content: ({
+                currentPage: Number,
+                currentRestaurants: (String)
+        }),
+        restaurants: String,
+})
+
+
+
+
+/*
+TODO Function
+add new users
+*/
+
+/*
+TODO Function
+Login users
+*/
+
+/*
+TODO Function
+create restaurant 
+*/
+
+/*
+TODO Function
+build hompage using homepage schema
+*/
+
+/*
+TODO Function
+
+*/
+/*
+TODO Function
+
+*/
 
 
 
@@ -49,10 +107,5 @@ document.addEventListener('DOMContentLoaded', function() {
                     window.location.href = 'Buissness.html';
             
         });
-    
-<<<<<<< HEAD
-});
 
-=======
 });
->>>>>>> 9328caa (updates)
