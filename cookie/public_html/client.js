733f74e471beba6d6c6f687e13727610bf417d36
jadel806 <100808410+jadel806@users.noreply.github.com>
2023-11-29 12:20:14 -0700

    added post/review

--- cookie/public_html/client.js
+++ cookie/public_html/client.js
@@ -17,6 +17,7 @@ function login() {
     if (text.startsWith('SUCCESS')) {
       alert(text);
       window.location.href = '/home.html';
+      localStorage.setItem("user", us); 
     } else {
       alert('failed');
     }
@@ -56,6 +57,61 @@ function createAccount() {
 function createBusiness(){
 
 
+}
+//Code to get the number of checked stars
+let stars = document.getElementsByClassName("star");
+let output = document.getElementById("output");
+
+function gfg(n) {
+    remove();
+    for (let i = 0; i < n; i++) {
+        if (n == 1) cls = "one";
+        else if (n == 2) cls = "two";
+        else if (n == 3) cls = "three";
+        else if (n == 4) cls = "four";
+        else if (n == 5) cls = "five";
+        stars[i].className = "star " + cls;
+    }
+    output.innerText = "Rating is: " + n + "/5";
+    localStorage.setItem("number", n);
+}
+
+function remove() {
+    let i = 0;
+    while (i < 5) {
+        stars[i].className = "star";
+        i++;
+    }
+}
+
+
+//Code to post a review
+function postReview() {
+    let starRating = localStorage.getItem("number");
+    let reviewText = document.getElementById('freeform').value;
+    let username = localStorage.getItem("user"); //change (used for testing purposes)
+
+    let info = {
+        starRating: starRating,
+        reviewText: reviewText,
+        username: username,
+    };
+
+    fetch('/add/review', {
+        method: 'POST',
+        body: JSON.stringify(info),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((text) => {
+        console.log(text);
+        // window.location.href = 'home.html';
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
 
 
