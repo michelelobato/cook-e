2a7cdbc80361ae19e7fb8e7a90e4f8c7d46ab15b
jadel806 <100808410+jadel806@users.noreply.github.com>
2023-12-06 23:11:37 -0700

    Update server.js

--- cookie/server.js
+++ cookie/server.js
@@ -5,31 +5,13 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
 const crypto = require('crypto');
-const path = require('path');
-const fs = require('fs').promises; 
-
 
 const db  = mongoose.connection;
-// const mongoDBURL =  'mongodb+srv://admin:ffucSFRiLP8txvNX@cluster0.l2iybs5.mongodb.net/?retryWrites=true&w=majority';
 const mongoDBURL = 'mongodb+srv://jasadelberg:HKjad473@cluster0.qr9yuh3.mongodb.net/';
-mongoose.connect(mongoDBURL, { useNewUrlParser: true })
-    const dataCollection = db.collection('Cluster0');
-
-    // API endpoint to get data
-    app.get('/api/data', (req, res) => {
-      dataCollection.find({}).toArray()
-        .then(results => {
-          res.json(results);
-        })
-        .catch(error => console.error(error));
-    });
-
-  .catch(error => {
-    console.error('MongoDB connection error:', error);
-  });
-
-
+mongoose.connect(mongoDBURL, { useNewUrlParser: true });
+db.on('error', () => { console.log('MongoDB connection error:') });
 
 //User schema
  var UserSchema = new mongoose.Schema({
@@ -44,24 +26,21 @@ mongoose.connect(mongoDBURL, { useNewUrlParser: true })
 var User = mongoose.model('User', UserSchema);
 
 //Business schema
-
 var BusinessSchema = new mongoose.Schema({
   name: String,
   username: String,
   password: String,
+  menu: String,
+  image: String,
   phone: String,
   email: String,
   address: String,
-  website: String,
-  restaurantTagline: String,
-  dishList1: String,
-  dishList2: String,
-  dishList3: String,
-  openingHours: String,
+  website: String, 
+  logo: String,
 });
-
-var Business = mongoose.model('Business', BusinessSchema);
-
+/**var User = mongoose.model('Business', BusinessSchema); */
+/**changed this since it is changing User instead of business*/
+var Business = mongoose.model('Business', BusinessSchema); 
 
 
 //Source from class slides
@@ -221,6 +200,7 @@ app.post('/add/review', async (req, res) => {
   }
 });
 
+
 app.post('/business/create', async (req, res) => {
   try {
     const {
@@ -263,70 +243,20 @@ app.post('/business/create', async (req, res) => {
 });
 
 
-app.get('/get/businesses/', async (req, res) => {
-  try {
-    const businessList = await Business.find().exec();
-    res.status(200).json(businessList);
-  }catch(error) {
-    console.error(error);
-    res.status(500).json({error: 'Unable to find restaurants'});
-  }
-  
+app.get('/get/reviews/:business', async (req, res) => {
+//gets all the business's reviews
+    try {
+        const business = req.params.business;
+        const reviews = await Review.find({ business: business }).exec();
+        res.status(200).json(reviews);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 
-app.use(express.static("public_html"))
-app.use('/img', express.static(__dirname + '/img'));
 
-// creating the dynamic website that takes information fromthe business form and creates a webpage in the given template
-
-app.get('/api/data', (req, res) => {
-    dataCollection.find({}).toArray()
-        .then(results => {
-            res.json(results);
-        })
-        .catch(error => console.error(error));
-});
-app.get('/Buissness.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Buissness.html'));
-});
-app.get('/BStyle.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'BStyle.css')); // Adjust the file name/path as necessary
-});
-
-app.post('/submit-form', async (req, res) => {
-    const formData = req.body; 
-    dataCollection.insertOne(formData)
-        .then(result => {
-            console.log('Form Data Saved');
-        })
-        .catch(error => console.error(error));
-
-    // Generate a new HTML page using the form data
-    const templatePath = path.join(__dirname, 'template.html');
-    let templateHTML = await fs.readFile(templatePath, 'utf8');
-    console.log(formData)
-    
-    app.get('/yelp.css', (req, res) => {
-        res.sendFile(path.join(__dirname, 'yelp.css')); // Adjust the file name/path as necessary
-    });
-
-    templateHTML = templateHTML.replace('<h1 id="restaurantName"></h1>', `<h1 id="restaurantName">${formData.BName}</h1>`);
-    templateHTML = templateHTML.replace('<h2 id="restaurantTagline"></h2>', `<h2 id="restaurantTagline">${formData.BrestaurantTagline}</h2>`);
-    templateHTML = templateHTML.replace('{{LocationArea}}', formData.BLocation);
-    templateHTML = templateHTML.replace('{{restaurantAddress}}', formData.BAddress);
-    templateHTML = templateHTML.replace('{{openingHours}}', formData.BopeningHours);
-    templateHTML = templateHTML.replace('{{website}}', formData.BWebsite);
-
-    //templateHTML = templateHTML.replace('<p id="openingHours">Open: {{openingHours}}</p>', `<p id="openingHours">Open :${formData.BopeningHours}</p>`);
-    templateHTML = templateHTML.replace('{{GoogleMapsURL}}', formData.BGoogleMapsURL);
-    templateHTML = templateHTML.replace('{{OrderLink}}', formData.BOrderLink);
-    templateHTML = templateHTML.replace('{{Pop1}}', formData.BdishesList1)
-    templateHTML = templateHTML.replace('{{Pop2}}', formData.BdishesList2)
-    templateHTML = templateHTML.replace('{{Pop3}}', formData.BdishesList3)
-
-    res.send(templateHTML);
-});
 
 const port = 80;
 app.listen(port, () => { console.log('server has started'); });
